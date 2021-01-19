819b0154b34dcf2c4c3fe6e1be0cf9dd8ffaeb02
Mark Peng <markpengisme@gmail.com>
2021-01-20 01:54:07 +0800

    social-blog 3.5 Flask-Moment

--- social-blog/hello.py
+++ social-blog/hello.py
@@ -1,10 +1,13 @@
+from datetime import datetime
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_script import Manager
+from flask_moment import Moment
 
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 manager = Manager(app)
 
 @app.errorhandler(404)
@@ -15,11 +18,11 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
-    
+
 @app.route('/')
 def index():
-    return render_template('index.html')
-
+    return render_template('index.html',
+                            current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
