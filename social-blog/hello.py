67a35925534a6ff292cb20f57dbdf6f400f8b1c1
Mark Peng <markpengisme@gmail.com>
2021-01-19 17:47:16 +0800

    social-blog 2.3  Flask-Script

--- social-blog/hello.py
+++ social-blog/hello.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_script import Manager
+ 
 app = Flask(__name__)
-
+manager = Manager(app)
 
 @app.route('/')
 def index():
@@ -10,3 +12,6 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello, {}!</h1>'.format(name)
+
+if __name__ == '__main__':
+    manager.run()
