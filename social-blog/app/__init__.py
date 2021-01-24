625dedafd92ab090256efda9b744c9cf948076a4
Mark Peng <markpengisme@gmail.com>
2021-01-24 17:20:44 +0800

    social-blog 8.3 Flask-Login

--- social-blog/app/__init__.py
+++ social-blog/app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from flask_mail import Mail, Message
+from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from config import config
 
 
@@ -10,6 +11,8 @@ bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
 
 
 def create_app(config_name):
@@ -21,6 +24,7 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
