0684c8e2d413ae47cdeccee3c4cbe1bcb393dc20
Mark Peng <markpengisme@gmail.com>
2021-01-27 17:42:32 +0800

    social-blog 9 User roles and permissions

--- social-blog/sblog.py
+++ social-blog/sblog.py
@@ -1,14 +1,14 @@
 import os
 from flask_migrate import Migrate
 from app import create_app, db
-from app.models import User, Role
+from app.models import User, Role, Permission
 
 app = create_app(os.getenv('FLASK_CONFIG', 'default'))
 migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db, User=User, Role=Role)
+    return dict(db=db, User=User, Role=Role, Permission=Permission)
 
 @app.cli.command()
 def test():
