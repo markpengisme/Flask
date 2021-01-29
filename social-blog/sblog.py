d6cb416a63ca2d1ae3d166efe1a8736358804255
Mark Peng <markpengisme@gmail.com>
2021-01-29 17:36:18 +0800

    social-blog 11.1 Blog posts

--- social-blog/sblog.py
+++ social-blog/sblog.py
@@ -1,14 +1,14 @@
 import os
 from flask_migrate import Migrate
 from app import create_app, db
-from app.models import User, Role, Permission
+from app.models import User, Role, Permission, Post
 
 app = create_app(os.getenv('FLASK_CONFIG', 'default'))
 migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db, User=User, Role=Role, Permission=Permission)
+    return dict(db=db, User=User, Role=Role, Permission=Permission, Post=Post)
 
 @app.cli.command()
 def test():
