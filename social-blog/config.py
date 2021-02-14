7f86fce6a1257c83816bbc5cfb9b8f4de1200e8f
Mark Peng <markpengisme@gmail.com>
2021-02-14 18:59:24 +0800

    social-blog 17.3 Heroku support with Gunicorn

--- social-blog/config.py
+++ social-blog/config.py
@@ -12,6 +12,7 @@ class Config:
     SBLOG_MAIL_SUBJECT_PREFIX = '[SBLOG]'
     SBLOG_MAIL_SENDER = 'SBLOG Admin <SBLOG@example.com>'
     SBLOG_ADMIN = os.environ.get('SBLOG_ADMIN')
+    SSL_REDIRECT = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_RECORD_QUERIES = True
     SBLOG_POSTS_PER_PAGE = 20
@@ -64,11 +65,30 @@ class ProductionConfig(Config):
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
 
+class HerokuConfig(ProductionConfig):
+    SSL_REDIRECT = True if os.environ.get('DYNO') else False
+
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+
+        # handle reverse proxy server headers
+        from werkzeug.middleware.proxy_fix import ProxyFix
+        app.wsgi_app = ProxyFix(app.wsgi_app)
+
+        # log to stderr
+        import logging
+        from logging import StreamHandler
+        file_handler = StreamHandler()
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
 
 config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'production': ProductionConfig,
+    'heroku': HerokuConfig,
 
     'default': DevelopmentConfig
 }
